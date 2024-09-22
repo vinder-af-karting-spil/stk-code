a8f8fe496d1e7124b0a8b8f65280d01ef27decca
Hauke Jensen <haukejensen745@gmail.com>
2024-09-22 12:26:27 +0200

    again /showcommands update

--- src/network/protocols/server_lobby.cpp
+++ src/network/protocols/server_lobby.cpp
@@ -7616,7 +7616,7 @@ unmute_error:
             L"/to|msg|dm|pm /slots|sl /public|pub|all "
             L"/listserveraddon|lsa /playerhasaddon /kick /playeraddonscore /serverhasaddon|sha /inform|ifm "
             L"/report /heavyparty|hp /mediumparty|mp /lightparty|lp /scanservers|online /mute /unmute /listmute /pole"
-            L" /bowlparty|bp /start /end" 
+            L" /bowlparty|bp /start /end /bug" 
         );
         chat->encodeString16(res);
         peer->sendPacket(chat, true/*reliable*/);
