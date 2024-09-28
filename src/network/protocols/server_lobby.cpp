f09d91f60757e852d680e19f5ca9ef536c460777
Hauke Jensen <haukejensen745@gmail.com>
2024-09-28 15:19:26 +0200

    adding /help (command)

--- src/network/protocols/server_lobby.cpp
+++ src/network/protocols/server_lobby.cpp
@@ -7968,7 +7968,12 @@ return;
     sendStringToPeer(msg, peer);
     return;
 }
-
+    else if (argv[0] == "help")
+{
+	std::string msg = "Use /help (the command you are wondering how it works) to check how the command works (not every command is included)."
+		sendStringToPeer(msg, peer);
+	return;
+}
     else if (argv[0] == "autoteams")
     {
         if ((noVeto || (player && player->getVeto() < 100)) && m_server_owner.lock() != peer)
