e5bfc8f93b4523f3270a7abe65066cd357e53549
DernisNW <dernisnw@neonw.su>
2024-10-04 07:31:20 +0500

    Chaos party also affects live join karts instantly

--- src/network/protocols/lobby_protocol.cpp
+++ src/network/protocols/lobby_protocol.cpp
@@ -259,6 +259,23 @@ void LobbyProtocol::addLiveJoiningKart(int kart_id, const RemoteKartInfo& rki,
     World::getWorld()->initTeamArrows(k);
     if (!k->getController()->isLocalPlayerController())
         k->setOnScreenText(rki.getPlayerName().c_str());
+    if (RaceManager::get()->getWorldTimedModifiers() & TIERS_TMODIFIER_CHAOSPARTY)
+    {
+        k->setPowerup(PowerupManager::POWERUP_NOTHING, 0);
+        k->setEnergy(0.0f);
+
+        RandomGenerator rg;
+        unsigned int pw = rg.get(
+                PowerupManager::POWERUP_MAX - 4 - 3) + 1;
+        if (pw >= PowerupManager::POWERUP_SWATTER)
+            pw++;
+        if (pw >= PowerupManager::POWERUP_SWITCH)
+            pw++;
+        if (pw >= PowerupManager::POWERUP_PLUNGER)
+            pw++;
+        k->setPowerup((PowerupManager::PowerupType)pw, 50);
+        k->setEnergy(100.0f);
+    }
 }   // addLiveJoiningKart
 
 //-----------------------------------------------------------------------------
