71af2aeaa99a176ff3961cabe60b34d2edbc7592
DernisNW <dernisnw@neonw.su>
2024-12-10 23:33:27 +0500

    When /setfield|/settrack has not specified number of laps, use default number of laps

--- src/network/protocols/server_lobby.cpp
+++ src/network/protocols/server_lobby.cpp
@@ -12019,6 +12019,9 @@ bool ServerLobby::forceSetTrack(std::string track_id,
                 {
                     if (ServerConfig::m_supertournament)
                         laps = fv.m_num_laps;
+                    else
+                     // apply default laps
+                        laps = t->getDefaultNumberOfLaps();
                 }
                 m_set_field = track_id;
                 m_set_laps = laps;
